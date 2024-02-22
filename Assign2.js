6d955fb578714ff398c76025b2ac1a7897ca9ffa
Rao-Owais <rao.owais2020@gmail.com>
2024-02-22 11:09:26 +0500

    Assignment#2

---
+++ Assign2.js
@@ -0,0 +1,48 @@
+//         TASK#1
+var username;
+
+//         TASK#2
+var myName = 'Owais Iqbal';
+
+//         TASK#3
+var message = 'Hello World';
+alert(message);
+
+ //        TASK#4
+var stdName = 'Ali Khan';
+var stdAge = '17 year old';
+var uni = 'SSUET';
+var course = 'Mobile & Cloud Computing - Module A';
+alert(stdName);
+alert(stdAge +"\n" +"Prevent this page from creating additional dialogs");
+alert(uni);
+alert(course +"\n" +"Prevent this page from creating additional dialogs");
+
+//         TASK#5
+var no_of_children = prompt("Enter Number of Children");
+var partners_Name = prompt("Enter Partner's Name");
+var geo_location = prompt("Enter Location");
+var job_title = prompt("Enter job Title");
+alert("You will be a "+ job_title +" in "+geo_location+", And married to "+partners_Name+" with "+no_of_children+" kids");
+
+//         TASK#6
+var email = 'rao.owais2020@gmail.com';
+alert("My Email Address Is "+email);
+
+//         TASK#7
+var book = "'A smarter way to learn javascript'";
+alert("I am trying to learn form the book "+book);
+
+//         TASK#8
+var stdName = 'Ali Khan';
+var stdAge = '17 year old';
+var uni = 'SSUET';
+var course = 'Mobile & Cloud Computing - Module A';
+alert("My Name Is "+stdName);
+alert("My Age Is "+stdAge);
+alert("I Study In "+uni);
+alert("I Take "+course+" Classes");
+
+//       TASK#9
+document.write("Yay! I can write HTML content through Javascript");
+
