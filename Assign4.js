7e9380e0645afd6abb266e48c7c2e6c3358a29e0
Rao-Owais <rao.owais2020@gmail.com>
2024-02-22 12:11:13 +0500

    Assignment#4

---
+++ Assign4.js
@@ -0,0 +1,25 @@
+//      TASK#1
+var firstname, firstName, first_name, $firstname, $firstName;
+
+// TASk#2 (legal)
+var firstname;
+var $firstName;
+var firstName;
+var first_name;
+var firstname2;
+
+//  (illegal)
+// var 12name;
+// var first Name;
+// var @name;
+// var %name;
+// var first-name;
+
+
+//   TASK#3
+document.write("Rules for naming JS variables:");
+document.write('<br/>'+"Variables name can only contain '$', '_', '123' and 'No space.'");
+document.write('<br/>'+"For Example $my_1stVariable.");
+document.write('<br/>'+"Variables must begin with a '$', '_', 'Character'.")
+document.write('<br/>'+"For Example $name, _name or name.");
+document.write('<br/>'+"Variables names are case sensitive");
