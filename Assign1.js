45c7f98993053f144088e1a86a3412eb3dc57da8
Rao-Owais <rao.owais2020@gmail.com>
2024-02-22 10:21:49 +0500

    Assignment#1

---
+++ Assign1.js
@@ -0,0 +1,19 @@
+
+//      TASK#1
+alert("Welcome to our Website");
+
+//      TASK#2
+alert("Error! Please Enter a Valid Password");
+
+//      TASK#3
+alert("Welcome to JS land..."+ "\n" + "Happy Coding");
+
+//      TASK#4
+alert("Welcome to JS land...");
+alert("Happy Coding"+"\n"+"Prevent this page from creating additional dialogs");
+
+//      TASK#5
+console.log("Hello... I can run JS through my web browser's console")
+
+
+
