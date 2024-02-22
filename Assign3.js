17c7a09a4a188311106e31756194457410122709
Rao-Owais <rao.owais2020@gmail.com>
2024-02-22 11:44:20 +0500

    Assignment#3

---
+++ Assign3.js
@@ -0,0 +1,17 @@
+//           TASK#1
+var age = 24;
+alert("I am "+age+" years old");
+
+//           TASk#2
+var time_count = 30;
+alert("You have visited this site "+time_count+" times");
+
+//          TAsk#3
+var birth_year = 1998;
+document.write("My birth year is " +birth_year+  '<br />' + "Data type of my declared variable is "+typeof(birth_year));
+
+//     TASk#4
+var v_name = prompt("Visitor's Name");
+var p_title = prompt("Product Title");
+var order = prompt("Quantity,how many product you want to order");
+alert(v_name+" ordered "+order+" "+p_title+"(s) on XYZ Clothing Store");
